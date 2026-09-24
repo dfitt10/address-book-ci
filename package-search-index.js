7540bacbc1bea6a960e8a69a080b51dc84373130
dfitt10 <51282455+dfitt10@users.noreply.github.com>
2026-09-24 07:03:38 +0000

    Deploying to gh-pages from @ dfitt10/address-book-ci@0728edf162c9b2f97ed5b5e1f6499bb0db0111fe 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"com.example.addressbook","l":"com.example.addressbook"},{"m":"com.example.addressbook","l":"com.example.addressbook.controller"},{"m":"com.example.addressbook","l":"com.example.addressbook.model"}];updateSearchResults();
